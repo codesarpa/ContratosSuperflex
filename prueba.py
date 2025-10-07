23ddfba406c6a7e7307b73a51256af593a1add1a
auxanalista <auxanalista@consuerte.com.co>
2025-10-07 08:47:14 -0500

    cedulas, 7/10/2025

--- prueba.py
+++ prueba.py
@@ -1,16 +1,21 @@
 import requests, urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-url = 'https://www.emsa-esp.com.co:441/factura/consulta_factura.php?cuenta=14420010'
+# facturta correcta
+# url = 'https://www.emsa-esp.com.co:441/factura/consulta_factura.php?cuenta=399274086'
+
+# factura erronea
+url = 'https://www.emsa-esp.com.co:441/factura/consulta_factura.php?cuenta=451245565'
 
 try:
   response = requests.get(url, timeout=30, verify=False)
   status_code = response.status_code
+  content_type = response.headers.get("Content-Type")
   txt_response = response.text
 except requests.exceptions.RequestException:
     status_code = None
 
-if txt_response == "No se genero la factura. Intente nuevamente o verifique los datos ingresados":
-   status_code = 300
-print(response)
-print(txt_response)
+if content_type != 'application/pdf':
+   print("factura buena")
+# print(response)
+print(response.headers.get("Content-Type"))
